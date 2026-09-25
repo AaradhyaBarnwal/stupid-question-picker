eb4402cdb50f098abaa5b883ee8b15a18dbb69ec
AaradhyaBarnwal <aaradhyabarnwal82@gmail.com>
2026-09-25 16:03:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let doneBtn = document.getElementById(".done");
+let inputValue = document.querySelector('.input');
+let quesDiv = document.querySelector(".ques");
+let genBtn = document.querySelector(".gen");
+
+doneBtn.addEventListener('click',()=>{
+    console.log("yo");
+})
